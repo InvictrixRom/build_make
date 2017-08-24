406c74431ea8e9a22301b7825d0a09543c6357ea
Prema Chand Alugu <premaca@gmail.com>
2017-08-24 15:53:16 +0530

    build: start gzosp

--- envsetup.sh
+++ envsetup.sh
@@ -634,7 +634,7 @@ function lunch()
     check_product $product
     if [ $? -ne 0 ]
     then
-        # if we can't find a product, try to grab it off the GZOSPGitHub
+        # if we can't find a product, try to grab it off the GZOSP GitHub
         T=$(gettop)
         cd $T > /dev/null
         vendor/gzosp/build/tools/roomservice.py $product
