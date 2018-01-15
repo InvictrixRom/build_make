2948be0ed6aae4d591d45f4d93539f69dbb897e5
Jacob McSwain <jacob.a.mcswain@gmail.com>
2018-01-14 23:43:09 -0600

    Enable roomservice

--- envsetup.sh
+++ envsetup.sh
@@ -733,23 +733,19 @@ function lunch()
     fi
 
     check_product $product
-#    if [ $? -ne 0 ]
-#    then
-#        # if we can't find the product, try to grab it from our github
-#        T=$(gettop)
-#        pushd $T > /dev/null
-#        if [[ $NO_ROOMSERVICE == true ]]; then
-#            echo "Roomservice turned off, type in 'export NO_ROOMSERVICE=false' if you want it back on"
-#        else
-#            if [[ $( grep -i "codeaurora" manifest/o8x_default.xml) ]]; then
-#                vendor/extras/tools/roomservice-caf.py $product
-#            else
-#                vendor/extras/tools/roomservice.py $product
-#            fi
-#        fi
-#        popd > /dev/null
-#        check_product $product
-#    fi
+    if [ $? -ne 0 ]
+    then
+        # if we can't find the product, try to grab it from our github
+        T=$(gettop)
+        pushd $T > /dev/null
+        if [[ $NO_ROOMSERVICE == true ]]; then
+            echo "Roomservice turned off, type in 'export NO_ROOMSERVICE=false' if you want it back on"
+        else
+            build/make/tools/roomservice.py $product
+        fi
+        popd > /dev/null
+        check_product $product
+    fi
 
 
     TARGET_PRODUCT=$product \
