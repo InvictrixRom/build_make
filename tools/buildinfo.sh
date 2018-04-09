7cbfd6e567b155e992ab2ba7009e753039a223a5
Jacob McSwain <jacob.a.mcswain@gmail.com>
2018-04-08 20:46:47 -0500

    GZOSP->Invictrix

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -60,6 +60,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
-echo "ro.gzosp.device=$GZOSP_DEVICE"
+echo "ro.invictrix.device=$INVICTRIX_DEVICE"
 
 echo "# end build properties"
