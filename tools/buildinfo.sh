RevengeOS/android_build

build: completely rework Makefile based on OmniROM and DirtyUnicorns
Change-Id: I1585ac131f513bfcfb7f8242403d5f152b8ca788

tools/buildinfo.sh
@@ -64,4 +64,7 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
+# Carbon properties
+echo "ro.carbon.device=$CARBON_DEVICE"
+
 echo "# end build properties"

InvictrixRom/build_make

tools/buildinfo.sh
@@ -58,4 +58,7 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
+# Invictrix properties
+echo "ro.invictrix.device=$INVICTRIX_DEVICE"
+
 echo "# end build properties"
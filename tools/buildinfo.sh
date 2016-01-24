c2b372130cd877046129633eadfd4e90416921ab
Christian Oder <myself5@carbonrom.org>
2016-01-24 19:51:14 +0100

    build: completely rework Makefile based on OmniROM and DirtyUnicorns
    Change-Id: I1585ac131f513bfcfb7f8242403d5f152b8ca788

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -58,4 +58,7 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
+# Invictrix properties
+echo "ro.invictrix.device=$INVICTRIX_DEVICE"
+
 echo "# end build properties"
