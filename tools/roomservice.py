omnirom/android_build

Fix grammar error in roomservice; Only add xml files in local_manifests
Change-Id: Ia132acddd2761ec4c1bea7fb510e3bea306ea9fc

tools/roomservice.py
@@ -96,8 +96,10 @@ def parse_device_directory(device_url, device):
 def iterate_manifests():
     files = []
     for file in os.listdir(local_manifest_dir):
-        files.append(os.path.join(local_manifest_dir, file))
+        if file.endswidth(".xml"):
+          files.append(os.path.join(local_manifest_dir, file))
     files.append('.repo/manifest.xml')
+
     for file in files:
         try:
             man = ES.parse(file)
@@ -197,7 +199,7 @@ def parse_device_from_folder(device):
     elif len(search) == 1:
         location = search[0]
     else:
-        print("you device can't be found in device sources..")
+        print("Your device can't be found in device sources..")
         location = parse_device_from_manifest(device)
     return location
 

InvictrixRom/build_make

tools/roomservice.py
@@ -101,7 +101,8 @@ def parse_device_directory(device_url,device):
 def iterate_manifests():
     files = []
     for file in os.listdir(local_manifest_dir):
-        files.append(os.path.join(local_manifest_dir, file))
+        if file.endswith(".xml"):
+            files.append(os.path.join(local_manifest_dir, file))
     files.append('.repo/manifest.xml')
     for file in files:
         try:
@@ -198,7 +199,7 @@ def parse_device_from_folder(device):
     elif len(search) == 1:
         location = search[0]
     else:
-        print("you device can't be found in device sources..")
+        print("Your device can't be found in device sources..")
         location = parse_device_from_manifest(device)
     return location
 